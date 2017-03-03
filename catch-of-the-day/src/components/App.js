84349ab75e54a8d531812f95fb8c97ace9920576
Ryan Milano <ryan@conveyedconcepts.com>
2017-03-02 16:23:47 -0800

    add fish component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+
 
 
 class App extends React.Component {
@@ -10,7 +12,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
-      this.loadSamples = this.loadSamples.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     // getintialstate
     this.state = {
       fishes: {},
@@ -39,6 +41,12 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object.keys(this.state.fishes)
+              .map(key => <Fish />)
+            }
+          </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddFishForm from './AddFishForm';
+
+class Fish extends React.Component {
+  render() {
+    return (
+      <li className="menu-fish">emoji</li>
+    )
+  }
+}
+
+export default Fish;
