59e5b41a336983ab7c702232e0d4aa5cfc497edd
Ryan Milano <ryan@conveyedconcepts.com>
2017-03-06 15:28:33 -0800

    add fish component

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import AddFishForm from './AddFishForm';
 
 class Fish extends React.Component {
   render() {
+    const { details } = this.props;
     return (
-      <li className="menu-fish">emoji</li>
+      <li className="menu-fish">
+        <img src={details.image} alt={this.props.details.name}/>
+        <h3 className="fish-name">
+        {details.name}
+        <span className="price">{details.price}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button>Add To Order</button>
+      </li>
+
     )
   }
 }
